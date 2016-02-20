99ddc36cc87d4325c9cc2e4db16d7d2b2c34ef34
cadytxu <cadytxu@gmail.com>
2016-02-20 18:13:53 +0000

    pass part 1 and part 2-1, 2-2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,7 +25,13 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(", ").each do |r|
+    if uncheck
+      uncheck "ratings_#{r}"
+    else
+      check "ratings_#{r}"
+    end
+  end
 end
 
 Then /I should see all the movies/ do
